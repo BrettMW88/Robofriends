b0c1d97dd269f991e3d5585dbda75a7bcf957ab3
BrettMW88 <63152735+BrettMW88@users.noreply.github.com>
2020-04-16 05:21:15 +0100

    Add files via upload

---
+++ build/precache-manifest.4f48016b18211db3176fe67aee90fe5e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6380c146817c94a6b765cdaa7e456ae5",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "1c82ff58af356e05d68c",
+    "url": "/robofriends/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "64c634cd0b562340ca9d",
+    "url": "/robofriends/static/css/main.bb02c4b8.chunk.css"
+  },
+  {
+    "revision": "1c82ff58af356e05d68c",
+    "url": "/robofriends/static/js/2.a10038f9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.a10038f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64c634cd0b562340ca9d",
+    "url": "/robofriends/static/js/main.44ae3023.chunk.js"
+  },
+  {
+    "revision": "8adbf24e3312ffc2ae3e",
+    "url": "/robofriends/static/js/runtime-main.403370f6.js"
+  }
+]);
