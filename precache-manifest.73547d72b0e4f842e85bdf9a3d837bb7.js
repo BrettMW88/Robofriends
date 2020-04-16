19fc6d44b5b46fd608c4281a38264d2577cdbdd8
Brett <brettworrall@icloud.com>
2020-04-16 04:54:36 +0100

    Updates

---
+++ precache-manifest.73547d72b0e4f842e85bdf9a3d837bb7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cd6f6dca0ad1efa065a78cabd8ed7e1",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "1c82ff58af356e05d68c",
+    "url": "/robofriends/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "547a361a4f5b588ba530",
+    "url": "/robofriends/static/css/main.21a05347.chunk.css"
+  },
+  {
+    "revision": "1c82ff58af356e05d68c",
+    "url": "/robofriends/static/js/2.a10038f9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.a10038f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "547a361a4f5b588ba530",
+    "url": "/robofriends/static/js/main.44ae3023.chunk.js"
+  },
+  {
+    "revision": "8adbf24e3312ffc2ae3e",
+    "url": "/robofriends/static/js/runtime-main.403370f6.js"
+  }
+]);
